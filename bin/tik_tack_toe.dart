be7112dd673d668c74b2800e4928c8e999a792db
Aseye Belinda <aseyebelinda78@gmail.com>
2023-05-06 01:01:32 +0000

    initial commit

---
+++ bin/tik_tack_toe.dart
@@ -0,0 +1,5 @@
+import 'package:tik_tack_toe/tik_tack_toe.dart' as tik_tack_toe;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tik_tack_toe.calculate()}!');
+}
---
+++ test/tik_tack_toe_test.dart
@@ -0,0 +1,8 @@
+import 'package:tik_tack_toe/tik_tack_toe.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
